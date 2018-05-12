fe8514777d60efa6122aae945eb340b069df31e6
yanlele <331393627@qq.com>
2018-05-12 19:43:47 +0800

    [+]导入模块

---
+++ book/01、python编程从入门到实践/08章、函数/06、将函数存储在模块中/making_pizzas.py
@@ -0,0 +1,5 @@
+import pizza1
+
+
+pizza1.make_pizza(16, 'pepperoni')
+pizza1.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
---
+++ book/01、python编程从入门到实践/08章、函数/06、将函数存储在模块中/pizza1.py
@@ -0,0 +1,4 @@
+def make_pizza(size, *toppings):
+    print('\n披萨的大小为： ' + str(size))
+    for topping in toppings:
+        print('- ' + topping)
