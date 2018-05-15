689c805a76ca21886dfa986e6b72a274461812e1
yanlele <331393627@qq.com>
2018-05-15 08:35:01 +0800

    [+]模块的分类和导入

--- book/01、python编程从入门到实践/09章、类/04、类的导入/car.py
+++ book/01、python编程从入门到实践/09章、类/04、类的导入/car.py
@@ -33,3 +33,26 @@ class Car():
     def increment_odometer(self, miles):
         """ 将里程表读数增加指定的量 """
         self.odometer_reading += miles
+
+
+class Battery():
+    def __init__(self, battery_size=70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print('这辆车还有：' + str(self.battery_size) + ' 千瓦/时的电量')
+
+    def get_range(self):
+        """打印出电瓶续航信息"""
+        if self.battery_size == 70:
+            range = 240
+        if self.battery_size == 85:
+            range = 270
+        message = '车辆的续航里程为： ' + str(range) + '千米'
+        print(message)
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
