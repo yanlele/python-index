3bc7b031f1f7e003f3911ed326c4cea3e08ad1a9
yanlele <331393627@qq.com>
2018-05-19 18:01:13 +0800

    [+]项目1-外星人入侵：在屏幕上绘制飞船

--- book/01、python编程从入门到实践/项目1、外星人入侵/alien_invasion.py
+++ book/01、python编程从入门到实践/项目1、外星人入侵/alien_invasion.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
-
+from ship import Ship
 
 def run_game():
     # 初始化屏幕对象
@@ -10,6 +10,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+
+    # 创建一艘飞船
+    ship = Ship(screen)
 
     # 开始游戏主循环
     while True:
@@ -21,6 +24,7 @@ def run_game():
 
         # 每次循环都需要重绘屏幕
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # 让最近回执的屏幕可见
         pygame.display.flip()
