c02989da814f9bc04f997e77704c0806b1ccc3cc
yanlele <331393627@qq.com>
2018-05-20 12:39:03 +0800

    [+]项目1-外星人入侵：创建 Bullet  类

--- book/01、python编程从入门到实践/项目1、外星人入侵/settings.py
+++ book/01、python编程从入门到实践/项目1、外星人入侵/settings.py
@@ -3,7 +3,6 @@ class Settings():
 
     def __init__(self):
         """初始化有的的设置"""
-
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
@@ -11,3 +10,10 @@ class Settings():
 
         # 飞船的设置 控制移动速度
         self.ship_speed_factor = 1.5
+
+        # 子弹的设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+
