ed6f4d0bd5eeec6ee07ba0d4f50fa5f3ab08423e
yanlele <331393627@qq.com>
2018-05-20 19:00:58 +0800

    [+]项目1-外星人入侵：检查外星人是否撞到了屏幕边缘

--- book/01、python编程从入门到实践/项目1、外星人入侵/alien.py
+++ book/01、python编程从入门到实践/项目1、外星人入侵/alien.py
@@ -25,7 +25,15 @@ class Alien(Sprite):
         """执行位置绘制外星人"""
         self.screen.blit(self.image, self.rect)
 
+    def check_edges(self):
+        """ 如果外星人位于屏幕边缘，就返回 True"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+
     def update(self):
         """ 向右移动外星人 """
-        self.x += self.ai_settings.alien_speed_factor
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
         self.rect.x = self.x
