cd5c1106ddb52aa707872e2f500f5fe333abdd29
yanlele <331393627@qq.com>
2018-05-20 12:03:48 +0800

    [+]项目1-外星人入侵：限制飞船的活动范围

--- book/01、python编程从入门到实践/项目1、外星人入侵/ship.py
+++ book/01、python编程从入门到实践/项目1、外星人入侵/ship.py
@@ -26,10 +26,13 @@ class Ship():
     def update(self):
         """根据移动标志调整"""
         #  更新飞船的 center 值，而不是 rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_right:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
+
+        #  根据 self.center 更新 rect 对象
+        self.rect.centerx = self.center
 
     def blitme(self):
         """在指定位置绘制飞船"""
