3a3b9a3c1c681d90ac2fe94a52c8a643af95e135
yanlele <331393627@qq.com>
2018-05-20 12:42:26 +0800

    [+]项目1-外星人入侵：将子弹存储到编组中

--- book/01、python编程从入门到实践/项目1、外星人入侵/alien_invasion.py
+++ book/01、python编程从入门到实践/项目1、外星人入侵/alien_invasion.py
@@ -1,9 +1,11 @@
 import sys
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+
 
 def run_game():
     # 初始化屏幕对象
@@ -15,6 +17,9 @@ def run_game():
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
 
+    # 创建一个用于存储子弹的编辑组
+    bullets = Group()
+
     # 开始游戏主循环
     while True:
         # 监听键盘和鼠标
@@ -22,6 +27,9 @@ def run_game():
 
         # 更新键盘操作来控制飞船
         ship.update()
+
+        # 将子弹存储到编组中
+        bullets.update()
 
         # 每次循环都需要重绘屏幕 而且 让最近回执的屏幕可见
         gf.update_screen(ai_settings, screen, ship)
