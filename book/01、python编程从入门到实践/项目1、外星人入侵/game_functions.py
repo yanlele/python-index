f92e765622ba26b313b19a2694f8bb09fb5c139d
yanlele <331393627@qq.com>
2018-05-20 18:08:17 +0800

    [+]项目1-外星人入侵：重构 create_fleet()

--- book/01、python编程从入门到实践/项目1、外星人入侵/game_functions.py
+++ book/01、python编程从入门到实践/项目1、外星人入侵/game_functions.py
@@ -75,18 +75,28 @@ def fire_bullet(ai_settings, screen, ship, bullets):
 
 
 def create_fleet(ai_settings, screen, aliens):
-    """创建外星人群"""
-    #  创建一个外星人，并计算一行可容纳多少个外星人
-    #  外星人间距为外星人宽度
+    """ 创建外星人群 """
+    #  创建一个外星人，并计算每行可容纳多少个外星人
     alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+
+    #  创建第一行外星人
+    for alien_number in range(number_aliens_x):
+        create_alien(ai_settings, screen, aliens, alien_number)
+
+
+def get_number_aliens_x(ai_settings, alien_width):
+    """ 计算每行可容纳多少个外星人 """
     available_space_x = ai_settings.screen_width - 2 * alien_width
     number_aliens_x = int(available_space_x/(2*alien_width))
+    return number_aliens_x
 
-    # 创建第一行外星人
-    for alien_number in range(number_aliens_x):
-        # 创建一个外星人并将其加入当前行
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2* alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    """ 创建一个外星人并将其放在当前行 """
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
