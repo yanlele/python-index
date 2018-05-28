8b3ebf4558dca19826a527d18e1ae2cbe9b1cf08
yanlele <331393627@qq.com>
2018-05-28 19:23:03 +0800

    [+]数据可视化：将字符串转换为数字值

---
+++ book/01、python编程从入门到实践/项目2、数据可视化/16章、下载数据/world_population2.py
@@ -0,0 +1,16 @@
+"""将字符串转换为数字值"""
+
+import json
+#  将数据加载到一个列表中
+filename = 'population_data.json'
+with open(filename) as f:
+    pop_data = json.load(f)     # 我们首先导入了模块 json ，以便能够正确地加载文件中的数据，然后，我们将数据存储在 pop_data 中
+
+# 打印每个国家2010年的人口数量
+for pop_dict in pop_data:       # 函数 json.load() 将数据转换为 Python 能够处理的格式，这里是一个列表
+    if pop_dict['Year'] == '2010':      # 们遍历 pop_data 中的每个元素。每个元素都是一个字典，包含四个键 — 值对，我们将每个字典依次存储在 pop_dict 中。
+        country_name = pop_dict['Country Name']     # 如果年份为 2010 ，我们就将与 'Country Name' 相关联的值存储到 country_name 中，并将与 'Value' 相关联的值存储在 population 中
+        population = int(float(pop_dict['Value']))      # 函数 float() 将字符串转换为小数，而函数 int() 丢弃小数部分，返回一个整数。现在，我们可以打印 2010 年的完整人口数据，不会导致错误了
+        print(country_name + ': ' + str(population))
+
+
