6d087193d649c5a4b4ec58d98cf6ced8beab8b92
yanlele <331393627@qq.com>
2018-06-02 13:50:40 +0800

    [+]开始使用css选择器来抓取页面数据

--- book/02、Scrapy分布式爬虫项目学习/ArticleSpider/ArticleSpider/spiders/jobbole_css_selecter.py
+++ book/02、Scrapy分布式爬虫项目学习/ArticleSpider/ArticleSpider/spiders/jobbole_css_selecter.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 import scrapy
 
 
@@ -8,7 +9,24 @@ class JobboleCssSelecterSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/110287/']
 
     def parse(self, response):
+        # title
         title = response.css('div.entry-header h1::text').extract()[0]
+
+        # 创建日期
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().strip().replace('·', '').strip()
+
+        # 点赞数
+        praise_nums = response.css("span.vote-post-up h10::text").extract()[0]
+
+        # 收藏数
+        fav_nums = response.css("span.bookmark-btn::text").extract()[0]
+        match_re = re.match('.*(\d+).*', fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+        else:
+            fav_nums = 0
+
+        # 评论数
 
 
         pass
