8e35bbaa3e621924d136f9186f98d3c5ca12ee1e
yanlele <331393627@qq.com>
2019-12-23 23:19:24 +0800

    feat: 添加链接mongogdb 的示例

--- 19年/12月/demo3/mongodb.py
+++ 19年/12月/demo3/mongodb.py
@@ -1,31 +1,36 @@
+﻿from pymongo import MongoClient
+
+# 建立MongoDB数据库连接
+client = MongoClient('localhost', 27017)
+db = client.mydb
+coll = db.get_collection("myCollection")
+# post_data = {  '_id': '222',  'item' : 'book',   'qty': 20
+# }
+# result = coll.insert_one(post_data)
+# print(result)
 
 
-client=MongoClient(host='10.90.9.101',port=27017)
-client=MongoClient(host='10.90.9.102',port=27018)
-client=MongoClient(host='10.90.9.101:27018,10.90.9.102:27018,10.90.9.103:27018')
-db.client.myDB
-db.conn.get_datebase("myDB")
-db=client.test
-coll=db.get_collection("mycollection")
-post_date={
-   '_id':'10',
-   'item':'book1',
-   'qty':18
-}
-result=coll.insert_one(post_date)
-print(result)
-post_1={'_id:'11','item':'book1','qty':18}
-post_2={'_id:'12','item':'book1','qty':18}
-post_3={'_id:'13','item':'book1','qty':18}
-
-new_result=coll.insert_many({post_1,post_2,post_3})
-
-find_post=coll.find_one({'item':'book'})
-find_post=coll.find({'item':'book1'})
-
-condition=('item':'book')
-pty{'$set':{'pty':22}}
-result=col.update(condition,pty)
-
-
-
+# '''插入多条'''
+# post_1 = {     '_id': '11',     'item' : 'book1',     'qty': 18 }
+# post_2 = {     '_id': '12',     'item' : 'book1',     'qty': 18 }
+# post_3 = {     '_id': '13',     'item' : 'book1',     'qty': 18 }
+# new_result = coll.insert_many([post_1,post_2,post_3])
+#
+# '''查询'''
+find_post = coll.find_one({'item': 'book'})
+find_posts = coll.find({'item': 'book1'})
+print("find_post", find_post)
+for i in find_posts:
+    print("find_posts", i)
+#
+# '''修改'''
+condition = {'item': 'book'}
+qty = {'$set': {'qty': 22}}
+result = coll.update(condition, qty)
+#
+# '''删除'''
+result = coll.remove({'item': 'book1'})
+result = coll.delete_one({'item': 'book'})
+result = coll.delete_many({'_id': '14'})
+# print(result)
+# '''
